9f369ad706ffd0cce67df8ec2ad7c3993861e30a
Micha Buchholz <mail@lean-stack.rocks>
2025-06-13 23:42:30 +0200

    chore: No compiler errors. Switch to Vue 3 mode.

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
           ...options,
           compilerOptions: {
             compatConfig: {
-              MODE: 2,
+              MODE: 3,
             },
           },
         };
