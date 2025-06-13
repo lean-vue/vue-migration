281af942d95a6c6bf175335f88c1fb98a17b4716
Micha Buchholz <mail@lean-stack.de>
2025-06-13 10:25:05 +0200

    chore: Add copy-modules-webpack-plugin. Unknown usage.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
+const CopyModulesPlugin = require("copy-modules-webpack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: (cfg) => {
+    if (process.env.NODE_ENV === "production") {
+      cfg.plugins.push(
+        new CopyModulesPlugin({
+          destination: "webpack_modules",
+        })
+      );
+    }
+  },
 });
