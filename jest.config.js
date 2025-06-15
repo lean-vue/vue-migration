7a6a8f1e835de29178f06c904665738805fff7c2
Micha Buchholz <mail@lean-stack.rocks>
2025-06-15 16:18:16 +0200

    chore: Refactor unit testing to run without vue-cli.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
+  testEnvironment: "jsdom",
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)",
+    "!**/tests/e2e/**",
+  ],
+  moduleFileExtensions: ["js", "json", "vue"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^[^.]+.vue$": "@vue/vue2-jest",
+    ".*\\.(js)$": "babel-jest",
+  },
 };
