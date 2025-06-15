b23ad9c3b03669189f826aad91cfa56844754b61
Micha Buchholz <mail@lean-stack.rocks>
2025-06-15 21:29:26 +0200

    chore: Align dev server port with vue cli. Set preview port.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue2";
 
 export default defineConfig({
+  server: {
+    port: 8080,
+  },
+  preview: {
+    port: 8088,
+  },
   plugins: [vue()],
   resolve: {
     alias: {
