6988d578c6f1753be73b9562fdb10701399e01a4
Micha <micha@egal.de>
2025-06-17 16:08:38 +0200

    chore: Upgrade popper.

--- src/views/plugins/PopperView.vue
+++ src/views/plugins/PopperView.vue
@@ -1,23 +1,18 @@
 <template>
-  <popper
-    trigger="clickToToggle"
-    :options="{
-      placement: 'bottom',
-      modifiers: { offset: { offset: '0,4px' } },
-    }"
-  >
-    <div class="popper">Popper Content</div>
+  <popper hover>
+    <template #content>
+      <div class="popper">Popper Content</div>
+    </template>
     <button slot="reference">Reference Element</button>
   </popper>
 </template>
 
 <script>
-import Popper from "vue-popperjs";
-import "vue-popperjs/dist/vue-popper.css";
+import Popper from "vue3-popper";
 
 export default {
   components: {
-    popper: Popper,
+    Popper,
   },
 };
 </script>
