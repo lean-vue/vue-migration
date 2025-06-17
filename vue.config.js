ed2a92059973cd418d0fea9a34c86bd192a5e9f7
Micha <micha@egal.de>
2025-06-17 16:56:29 +0200

    Remove hydration warning.

--- vue.config.js
+++ vue.config.js
@@ -20,6 +20,15 @@ module.exports = defineConfig({
           },
         };
       });
+
+    config.plugin("define").tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_OPTIONS_API__: "true",
+        __VUE_PROD_DEVTOOLS__: "false",
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false",
+      });
+      return definitions;
+    });
   },
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === "production") {
